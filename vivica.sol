03544d42c00d7e88d21b5ba2028383c2345f36f1
oluborodevictor <oluborodevictor110@gmail.com>
2025-05-15 22:45:44 +0100

    changes

--- vivica.sol
+++ vivica.sol
@@ -6,14 +6,31 @@ contract MyToken {
     string public name;
     string public symbol;
     uint8 public decimals;
+    uint private _totalSupply;
+    address public owner;
 
     constructor () {
         name = "Vivica";
         symbol = "VA";
         decimals = 18;
+        owner = msg.sender;
+        _mint(msg.sender, 1000 * 10 ** uint(decimals));
     }
 
-    function getTokename () public view returns (string memory) {
+    mapping(address account => uint256) public _balances;
+
+    function _mint (address account, uint256 amount) internal {
+        _balances[account]+= amount;
+        _totalSupply += amount;
+    }
+
+    function mint (address account, uint256 amount) public {
+        require(msg.sender == owner, "Not allowed!");
+        uint rawAmount = amount * 10 ** uint(decimals);
+        _mint(account, rawAmount);
+    }
+
+    function getTokenName () public view returns (string memory) {
         return name;
     }
 
@@ -25,12 +42,12 @@ contract MyToken {
         return decimals;
     }
 
-    function totalSupply() public {
-
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
     }
 
-    function balanceOf() public {
-
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
     }
 
     function transfer () public {
